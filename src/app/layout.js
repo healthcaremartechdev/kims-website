a76100866a712b1c27c2a18278816aeffd94dad2
Sourav Bishai <106469372+techCodeSB@users.noreply.github.com>
2026-04-24 14:45:45 +0530

    new speciality menu add; alumini page changes 3

--- src/app/layout.js
+++ src/app/layout.js
@@ -70,10 +70,10 @@ export default async function RootLayout({ children }) {
         <link rel="stylesheet" href="/css/custom.css" />
 
 
-        <link rel="stylesheet" href="/css/alumini.css" />
+        {/* <link rel="stylesheet" href="/css/alumini.css" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;500;600;700;800&family=Poppins:wght@500;600;700;800&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;500;600;700;800&family=Poppins:wght@500;600;700;800&display=swap" rel="stylesheet" /> */}
 
 
         {/* All Scripts */}
